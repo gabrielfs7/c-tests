e9005205eaf50ed93a20532a72f7542324ca58f9
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2018-05-05 21:09:12 +0200

    file search with C

--- CProjects/Step09/02_FileStats/main.c
+++ CProjects/Step09/02_FileStats/main.c
@@ -5,24 +5,37 @@
 #define EMPTYFILENAME "empty.txt"
 #define INVALIDFILENAME "not_here.txt"
 
-void linecount(char *fn) {
+int linecount(char *file_path)
+{
 	int numlines = 0;
 	char line[MAXSTRLEN];
-	FILE *fp = fopen ( fn, "r" );
-	if ( fp != 0 ) {		
-		while ( fgets ( line, sizeof(line), fp ) != 0 ) {
-			numlines++;
-		}
-		fclose ( fp );
-		printf("%s contains %d lines of text.\n", fn, numlines);
-	} else {
-		printf( "File %s cannot be opened!\n", fn);
-	}
+	FILE *file_pointer = fopen (file_path, "r");
+
+    // If files does not exists or has no reading permissions
+	if (file_pointer == 0) {
+        printf("File %s cannot be opened!\n", file_path);
+
+        return 0;
+    }
+
+    // While file has lines
+    while (fgets(line, sizeof(line), file_pointer) != 0) {
+        numlines++;
+    }
+
+    // Close the file
+    fclose(file_pointer);
+
+    printf("%s contains %d lines of text.\n", file_path, numlines);
+
+    return 1;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	linecount(FILENAME);
 	linecount(EMPTYFILENAME);
 	linecount(INVALIDFILENAME);
+
 	return 0;
 }
--- CProjects/Step09/03_FileSearchStr/main.c
+++ CProjects/Step09/03_FileSearchStr/main.c
@@ -4,42 +4,56 @@
 #define FILENAME "sonnet.txt"
 #define MAXSTRLEN 200
 
-// returns index (0-based) of first char of searchstring in sourcestring
-// or -1 if searchstring is not found
-static int searchstring(char searchstr[], char sourcestr[]) {
-	char *ptrtostr;
-	int foundat;
-	foundat = -1;
-	ptrtostr = strstr( sourcestr, searchstr );
-	if ( ptrtostr != 0 ) {
-		foundat = (int)((ptrtostr - sourcestr));
+/**
+ * Returns index (0-based) of first char of searchstring in sourcestring
+ * or -1 if searchstring is not found.
+ */
+static int searchstring(char searchstr[], char sourcestr[])
+{
+	char *pointer_to_string = strstr(sourcestr, searchstr);
+
+	if (pointer_to_string != 0) {
+		return (int) (pointer_to_string - sourcestr);
 	}
-	return foundat;
+
+	return -1;
 }
 
-void findstrings(char *fileName, char *ss) {
-	FILE *f;
+int findstrings(char *fileName, char *ss)
+{
+    // open file read only
+	FILE *file_pointer = fopen(fileName, "r");
 	int count;
 	char line[MAXSTRLEN];
 
-	f = fopen(fileName, "r");			// open file read only
-	if (f == 0) {
+    // File does not exists or is not readable
+	if (file_pointer == 0) {
 		printf("Can't open the file: '%s'\n", FILENAME);
-	} else {
-		count = 0;						// initialize the count
-		while ( fgets ( line, MAXSTRLEN, f ) != 0 ) {
-			if (searchstring(ss, line) >= 0) {
-				count++;
-			}
-		}
-		printf ("'%s' was found in %d lines\n", ss, count);
-		fclose(f);						// close it
+
+        return -1;
 	}
+
+    // initialize the count
+    count = 0;
+
+    while (fgets(line, MAXSTRLEN, file_pointer) != 0) {
+        if (searchstring(ss, line) >= 0) {
+            count++;
+        }
+    }
+
+    printf ("'%s' was found in %d lines\n", ss, count);
+
+    // Close file
+    fclose(file_pointer);
+
 }
 
-int main(int argc, char **argv) {
-	findstrings(FILENAME, "then");
-	findstrings(FILENAME, "my");
-	findstrings(FILENAME, "snodgrass");
+int main(int argc, char **argv)
+{
+	findstrings(FILENAME, "line 1");
+	findstrings(FILENAME, "line 2");
+	findstrings(FILENAME, "line");
+
 	return 0;
 }
