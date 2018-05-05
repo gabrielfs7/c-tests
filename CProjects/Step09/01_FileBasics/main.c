7361864d8f96593524d21dae25b56e3c4e19ac85
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2018-05-05 20:34:40 +0200

    handling files

--- CProjects/Step09/01_FileBasics/main.c
+++ CProjects/Step09/01_FileBasics/main.c
@@ -3,45 +3,75 @@
 #define MAXSTRLEN 200
 #define FILENAME "test.txt"
 
-void readlines() {
-	FILE *fp = fopen( FILENAME, "r" );
+void readlines()
+{
+    /**
+     *
+     * Open a file in READ mode.
+     *
+     */
+	FILE *file_pointer = fopen(FILENAME, "r");
+
 	char line[MAXSTRLEN];
-	if ( fp != 0 ) {
-		while ( fgets ( line, sizeof( line ), fp ) != 0 ) {
-			fputs ( line, stdout );
+
+	if (file_pointer != 0) {
+        /***
+         * While has lines, move line contents to "line" variable end then
+         * output it to stdout
+         */
+		while (fgets (line, sizeof(line), file_pointer) != 0) {
+			fputs (line, stdout);
 		}
-		fclose( fp );
+
+		fclose(file_pointer);
 	} else {
-		printf( "File %s cannot be opened!", FILENAME );
+		printf("File %s cannot be opened!", FILENAME);
 	}
 }
 
-void writelines() {
-	FILE *fp;
-	fp = fopen( FILENAME, "w" );
-	fputs( "Hello world\n", fp );
-	fputs( "Goodbye Mars\n", fp );
-	fclose( fp );
+void writelines()
+{
+    /***
+     * The w mode will erase file content.
+     */
+	FILE *file_pointer = fopen(FILENAME, "w");
+
+    /***
+     * Write content in file and move the cursos to the next line.
+     */
+	fputs("Hello world\n", file_pointer);
+	fputs("Goodbye Mars\n", file_pointer);
+
+	fclose(file_pointer);
 }
 
-void clearfile() {
-	FILE *fp;
-	fp = fopen( FILENAME, "w" );
-	fclose( fp );
+void clearfile()
+{
+	FILE *file_pointer;
+
+    /***
+     * The "w" mode erease the file content and move the cursos to first line.
+     */
+	file_pointer = fopen(FILENAME, "w");
+
+	fclose(file_pointer);
 }
 
-void deletefile() {
-	if ( remove( FILENAME ) == 0 ) {
+void deletefile()
+{
+	if (remove(FILENAME) == 0) {
 		printf( "%s file deleted.\n", FILENAME );
 	} else {
-		printf( "Unable to delete the file: %s.\n", FILENAME );
+		printf("Unable to delete the file: %s.\n", FILENAME);
 	}
 }
 
-int main( int argc, char **argv ) {
-	// deletefile();
-	// clearfile();
+int main( int argc, char **argv )
+{
+	deletefile();
 	writelines();
 	readlines();
+    clearfile();
+
 	return 0;
 }
